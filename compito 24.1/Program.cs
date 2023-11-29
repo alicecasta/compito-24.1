cce27b74983bd03949bce4f8f4041dd3238d0562
casta <casta@ALICELABELLA>
2023-11-29 15:37:55 +0100

    Aggiungere i file di progetto.

---
+++ compito 24.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compito_24._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            quadro l = new quadro();
+            Console.WriteLine("inserire il lato");
+            l.Lato = float.Parse(Console.ReadLine());
+            Console.WriteLine( "l'area è {0}", (l.Lato)*(l.Lato));
+            Console.ReadLine();
+        }
+    
+    }
+}
---
+++ compito 24.1/quadro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compito_24._1
+{
+     class quadro
+    {
+        public float Lato { get; set; }
+
+    }
+}
